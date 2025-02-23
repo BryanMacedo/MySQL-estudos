d2c012c3e2f9d1b635784177cea99a25765f0d04
Bryan Macedo <contato.bryansm@hotmail.com>
2025-02-23 08:54:07 -0300

    Transações agora não são executadas pela metade no banco de dados, agora ou as ações ocorrem por completo, ou não ocorrem.

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,32 +1,46 @@
 package application;
 
 import db.DB;
-import db.DbIntegrityException;
+import db.DbException;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Program {
     public static void main(String[] args) {
         Connection conn = null;
-        PreparedStatement st = null;
+        Statement st = null;
 
         try {
             conn = DB.getConnection();
-            st = conn.prepareStatement(
-                    "DELETE FROM department " // DELETE TEM Q TER WHERE
-                    + "WHERE "
-                    +"Id = ?"
-            );
-            st.setInt(1, 2);
+            conn.setAutoCommit(false);
 
-            int rowsAffected = st.executeUpdate();
-            System.out.println("Done! Rows affected: " + rowsAffected);
-        }catch (SQLException e){
-            throw new DbIntegrityException(e.getMessage());
+            st = conn.createStatement();
 
-        }finally {
+            int rows1 = st.executeUpdate("UPDATE seller SET BaseSalary = 2090 WHERE DepartmentId = 1");
+
+//            int x = 1;
+//            if (x < 2) {
+//                throw new SQLException("FAKE ERROR");
+//            }
+
+            int rows2 = st.executeUpdate("UPDATE seller SET BaseSalary = 3090 WHERE DepartmentId = 2");
+
+            conn.commit();
+
+            System.out.println("rows1: " + rows1);
+            System.out.println("rows2: " + rows2);
+
+        } catch (SQLException e) {
+            try {
+                conn.rollback();
+                throw new DbException("Transaction rolled back! Caused by: " + e.getMessage());
+            } catch (SQLException ex) {
+                throw new DbException("Error trying to rollback! Caused by: " + ex.getMessage());
+            }
+
+        } finally {
             DB.closeStatement(st);
             DB.closeConnection();
         }
